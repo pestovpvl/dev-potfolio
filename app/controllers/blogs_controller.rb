9348510cc3a08fa1c0c34ae97d0db6f666b18d5d
pestovpvl <pestovpvl@mail.ru>
2018-05-21 22:53:29 -0400

    Added nav bar and added default header content

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,11 +5,14 @@ class BlogsController < ApplicationController
   # GET /blogs.json
   def index
     @blogs = Blog.all
+    @page_title = "Blogszz"
   end
 
   # GET /blogs/1
   # GET /blogs/1.json
   def show
+    @page_title = @blog.title
+    @page_keywords = @blog.body
   end
 
   # GET /blogs/new
---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,11 @@
+module DefaultPageContent
+    extend ActiveSupport::Concern
+        
+    included do
+        before_action :set_page_defaults
+    end
+    def set_page_defaults
+        @page_title = "My portfolio"
+        @page_keywords = "Pavel Pestau"
+    end
+end
